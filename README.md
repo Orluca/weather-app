aebebaad8f2f54cb0fe6de9ba37461a89debb7e7
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-07-04 17:04:23 +0200

    Added an additional rain bar chart

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ A simple weather app created with Vanilla JS and ChartJS.
 - [] Light/Dark mode toggle
 - [] For the big overcast symbol it could be nicer to use a more 'sophisticated' image/symbol with some color instead of Font Awesome
 - [] Temperature line changes color the hotter/colder it gets (from blue to violet/pink)
-- [] Create more column divisions for the app container, so that the forecast chart can be made a little bit bigger
+- [x] Create more column divisions for the app container, so that the forecast chart can be made a little bit bigger
--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 Chart.defaults.font.family = "sans-serif, 'FontAwesome'";
 
+const temperatures = [16.29, 14, 11.27, 16.51, 22.48, 25.49, 26.79, 24.61, 18.72, 15.82, 14.61, 20.42, 27.41, 30.61, 30.95, 28.03, 20.59, 17.91, 14.7, 19, 23.32, 25.01, 28.61, 25.23, 17.75, 13.71, 11.83, 16.64, 20.79, 24.21, 25.88, 23.58, 16.31, 12.94, 11.29, 16.37, 21.51, 25.69, 26.44, 22.15];
 const times = ["17:00", "20:00", "23:00", "02:00", "05:00", "08:00", "11:00", "14:00", "17:00", "20:00", "23:00", "02:00", "05:00", "08:00", "11:00", "14:00", "17:00", "20:00", "23:00", "02:00", "05:00", "08:00", "11:00", "14:00", "17:00", "20:00", "23:00", "02:00", "05:00", "08:00", "11:00", "14:00", "17:00", "20:00", "23:00", "02:00", "05:00", "08:00", "11:00", "14:00"];
 const overcasts = ["\uf76c", "☀", "☁", "🌧", "☁", "☀", "☁", "🌧", "🌧", "☁", "☀", "☁", "☀", "☁", "☁", "☁", "☁", "☁", "☀", "☁", "☁", "☁", "☀", "🌦", "☁", "🌧", "☁", "🌧", "🌧", "🌧", "☁", "☁", "🌧", "🌧", "☁", "☁", "☁", "☁", "☁", "☁"];
-
+const rain = [0, 0, 0, 0, 0, 0.25, 0.54, 0.32, 0, 0, 0, 0, 0, 0.23, 0, 0, 0, 0.17, 1.2, 0.6, 0.15, 1, 0.73, 0.11, 0, 0, 0, 0, 0, 0, 0, 0, 0.4, 0.16, 0, 0.1, 0, 0, 0.16, 0.3];
 // Chart.register.ChartDataLabels;
 Chart.register(ChartDataLabels);
 
@@ -13,11 +14,17 @@ const myChart = new Chart(ctx, {
     labels: times,
     datasets: [
       {
-        label: "# of Votes",
-        data: [16.29, 14, 11.27, 16.51, 22.48, 25.49, 26.79, 24.61, 18.72, 15.82, 14.61, 20.42, 27.41, 30.61, 30.95, 28.03, 20.59, 17.91, 14.7, 19, 23.32, 25.01, 28.61, 25.23, 17.75, 13.71, 11.83, 16.64, 20.79, 24.21, 25.88, 23.58, 16.31, 12.94, 11.29, 16.37, 21.51, 25.69, 26.44, 22.15],
-        // backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(153, 102, 255, 0.2)", "rgba(255, 159, 64, 0.2)"],
-        // borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)", "rgba(75, 192, 192, 1)", "rgba(153, 102, 255, 1)", "rgba(255, 159, 64, 1)"],
+        type: "line",
+        label: "Temperatures",
+        data: temperatures,
         borderWidth: 1,
+      },
+      {
+        type: "bar",
+        label: "Rain",
+        data: rain,
+        yAxisID: "rainAxis",
+        backgroundColor: "blue",
       },
     ],
   },
@@ -30,6 +37,14 @@ const myChart = new Chart(ctx, {
             return value + "°C";
           },
         },
+      },
+      rainAxis: {
+        position: "right",
+        beginAtZero: true,
+        grid: {
+          drawOnChartArea: false,
+        },
+        suggestedMax: 10,
       },
       x: {
         display: true,
@@ -46,7 +61,6 @@ const myChart = new Chart(ctx, {
       x2: {
         type: "category",
         position: "top",
-
         ticks: {
           callback: function (value, index, ticks) {
             return overcasts[value];
@@ -55,6 +69,10 @@ const myChart = new Chart(ctx, {
             size: "15vw",
             padding: 0,
           },
+        },
+        grid: {
+          drawOnChartArea: false,
+          drawTicks: false,
         },
       },
     },
@@ -65,7 +83,6 @@ const myChart = new Chart(ctx, {
       annotation: {
         annotations: {
           day1: {
-            // Indicates the type of annotation
             type: "line",
             xMin: 7 / 3,
             xMax: 7 / 3,
@@ -87,7 +104,6 @@ const myChart = new Chart(ctx, {
             },
           },
           day2: {
-            // Indicates the type of annotation
             type: "line",
             xMin: 7 / 3 + 8,
             xMax: 7 / 3 + 8,
@@ -104,7 +120,6 @@ const myChart = new Chart(ctx, {
             },
           },
           day3: {
-            // Indicates the type of annotation
             type: "line",
             xMin: 7 / 3 + 16,
             xMax: 7 / 3 + 16,
@@ -121,7 +136,6 @@ const myChart = new Chart(ctx, {
             },
           },
           day4: {
-            // Indicates the type of annotation
             type: "line",
             xMin: 7 / 3 + 24,
             xMax: 7 / 3 + 24,
@@ -138,7 +152,6 @@ const myChart = new Chart(ctx, {
             },
           },
           day5: {
-            // Indicates the type of annotation
             type: "line",
             xMin: 7 / 3 + 32,
             xMax: 7 / 3 + 32,
@@ -158,7 +171,9 @@ const myChart = new Chart(ctx, {
       },
       datalabels: {
         color: "white",
-        formatter: Math.round,
+        formatter: function (value, context) {
+          return context.dataset.type === "line" ? Math.round(value) : ""; // Datalabels should only be displayed for the temperature chart, not for the rain bars
+        },
       },
     },
     responsive: true,
